0a73d88a95f8105e0e7153bbff03a9910f64195b
承諾 <klnaserzy@gmail.com>
2024-10-25 11:43:34 +0800

    修改Nav component的位置

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <script setup>
   import EnterWebTitle from '@/components/EnterWebTitle.vue';
+  import Nav from '@/components/Nav.vue';
   import IntroduceProduct from '@/components/IntroduceProduct.vue';
 </script>
 
 <template>
   <div class="main">
+    <Nav />
     <EnterWebTitle />
     <IntroduceProduct />
+    <footer style="width: 100vw; height: 0.5vh;"></footer>
   </div>
 </template>
 
